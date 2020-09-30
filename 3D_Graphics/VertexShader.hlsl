a0ed34ef15c37a533c4a5343d67b879a83cb797a
AEinstein <adereinstein1@gmail.com>
2020-09-30 10:22:25 -0700

    3d cube boi

--- 3D_Graphics/VertexShader.hlsl
+++ 3D_Graphics/VertexShader.hlsl
@@ -1,18 +1,9 @@
-struct VSOut
-{
-    float3 color : Color;
-    float4 position : SV_Position;
-};
-
-cbuffer CBuff
+cbuffer CBuf
 {
     matrix transform;
 };
 
-VSOut main( float2 pos : Position, float3 color : Color )
+float4 main(float3 pos : Position) : SV_Position
 {
-    VSOut vso;
-    vso.color = color;
-    vso.position = mul(float4(pos.x, pos.y, 0.0f, 1.0f), transform);
-    return vso;
+    return mul(float4(pos, 1.0f), transform);
 }
