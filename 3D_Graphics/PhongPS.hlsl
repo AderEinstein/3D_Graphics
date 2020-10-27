9fece317ab9741b621313610a6f6f22a5774cfb3
AEinstein <adereinstein1@gmail.com>
2020-10-27 06:02:06 -0700

    fix diffuse attenuation & adjust ambient lighting

--- 3D_Graphics/PhongPS.hlsl
+++ 3D_Graphics/PhongPS.hlsl
@@ -4,13 +4,12 @@ cbuffer LightCBuf
 };
 
 static const float attConst = 1.0f;
-static const float attLin = 1.0f;
-static const float attQuad = 1.0f;
+static const float attLin = 0.027f;
+static const float attQuad = 0.0028f;
 static const float3 materialColor = { 0.7f,0.7f,0.9f };
-static const float3 ambient = { 0.15f,0.15f,0.15f };
+static const float3 ambient = { 0.05f,0.05f,0.05f };
 static const float3 diffuseColor = { 1.0f,1.0f,1.0f };
 static const float diffuseIntensity = 1.0f;
-
 
 float4 main( float3 worldPos : Position,float3 n : Normal ) : SV_Target
 {
@@ -19,9 +18,9 @@ float4 main( float3 worldPos : Position,float3 n : Normal ) : SV_Target
 	const float distToL = length( vToL );
 	const float3 dirToL = vToL / distToL;
 	// Diffuse attenuation
-	const float att = attConst + attLin * distToL + attQuad * (distToL * distToL);
+    const float att = 1 / (attConst + attLin * distToL + attQuad * (distToL * distToL)); // Inverse relationship between distance and light intensity
 	// Diffuse intensity
 	const float3 diffuse = diffuseColor * diffuseIntensity * att * max( 0.0f,dot( dirToL,n ) );
 	// Final color
-	return float4(saturate( diffuse + ambient ),1.0f);
+	return float4(saturate( diffuse + ambient ), 1.0f);
 }
